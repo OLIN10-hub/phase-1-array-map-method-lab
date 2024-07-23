0e450ed12396a8c32048c3db4dfb59beb3ebd742
OLIN10-hub <muriuolin8@gmail.com>
2024-07-24 02:46:32 +0300

    array map

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(words => {
+      return words.split(' ').map(word => {
+          return word.charAt(0).toUpperCase() + word.slice(1)
+      }).join(' ');
+  });
 }
